adfbb36a52ebc193aa4b41a07ed106ff75d93540
Kristiyan96 <kristiyan.c@gmail.com>
2021-10-17 18:28:40 +0300

    Edit left sidebar

---
+++ src/navigation/vertical/common.js
@@ -0,0 +1,18 @@
+export default [
+  {
+    header: "Other",
+  },
+  {
+    title: "Support",
+    route: "support",
+  },
+  {
+    title: "FAQ",
+    route: "pages-faq",
+  },
+  {
+    title: "Blog",
+    route: "pages-blog-list",
+    icon: "MessageSquareIcon",
+  },
+];
--- src/navigation/vertical/index.js
+++ src/navigation/vertical/index.js
@@ -30,12 +30,8 @@ tag
 tagVariant
 
 */
-import dashboard from './dashboard'
 import appsAndPages from './apps-and-pages'
-import others from './others'
-import chartsAndMaps from './charts-and-maps'
-import uiElements from './ui-elements'
-import formAndTable from './forms-and-table'
+import common from './common'
 
 // Array of sections
-export default [...dashboard, ...appsAndPages, ...uiElements, ...formAndTable, ...chartsAndMaps, ...others]
+export default [...appsAndPages, ...common]
