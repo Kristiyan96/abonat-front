5e249dfc520c41bcbc30a73a9e4d18d2012d13ed
Kristiyan96 <kristiyan.c@gmail.com>
2021-10-25 22:34:05 +0300

    Create landing page

---
+++ src/views/pages/landing/Landing.vue
@@ -0,0 +1,24 @@
+<template>
+  <section>
+    Landing page, No layout
+  </section>
+</template>
+
+<script>
+export default {
+  components: {
+  },
+  data() {
+    return {
+    };
+  },
+  created() {
+  },
+  methods: {
+  },
+};
+</script>
+
+<style lang="scss">
+@import "@core/scss/vue/pages/page-landing.scss";
+</style>
